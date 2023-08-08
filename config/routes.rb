1aade17b4481ba09c31ad15d9a0e25d0c333561a
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-08 16:34:12 +0400

    add destroy to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :questions, only: %i[index new edit create update]
+  resources :questions, only: %i[index new create edit update destroy]
 
   root "pages#index"
 end
