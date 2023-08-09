301a2dcab872c7fb36b7e327df9359f4957f6f1b
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-09 16:40:30 +0400

    add flash messages to Q controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,6 +17,7 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new question_params
     if @question.save
+      flash[:success] = "Question created!"
       redirect_to questions_path
     else
       render :edit, status: :unprocessable_entity
@@ -30,6 +31,7 @@ class QuestionsController < ApplicationController
   def update
     @question = Question.find_by id: params[:id]
     if @question.update question_params
+      flash[:success] = "Question updated!"
       redirect_to questions_path
     else
       render :new, status: :unprocessable_entity
@@ -39,6 +41,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question = Question.find_by id: params[:id]
     @question.destroy
+    flash[:success] = "Question deleted!"
     redirect_to question_path
   end
 
