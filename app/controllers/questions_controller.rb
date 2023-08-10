be54c9f56b4ad561aa67fbd5f2584fc675b90ba2
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-10 13:34:20 +0400

    update redirect to

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -43,7 +43,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
     flash[:success] = "Question deleted!"
-    redirect_to question_path
+    redirect_to questions_path
   end
 
   private
