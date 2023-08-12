4c0b648796adb88abf1fda4d1a0e9e3c9cf2359a
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-12 17:24:42 +0400

    moved time method from models to decorators

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
-
-  def formatted_created_at
-    created_at.strftime("%Y-%m-%d %H:%M:%S")
-  end
 end
