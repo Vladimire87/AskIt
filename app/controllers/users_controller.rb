a75154e575231ad050e5bb6df3beea4da2e08568
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-08-14 13:38:12 +0400

    add before actions and set user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :require_no_authentication
+  before_action :require_no_authentication, only: %i[new create]
+  before_action :require_authentication, only: %i[edit update]
+  before_action :set_user!
 
   def new
     @user = User.new
@@ -24,6 +26,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user!
+    @user = User.find params[:id]
+  end
 
   def user_params
     params.require(:user).permit(:email, :name, :password, :password_confirmation)
